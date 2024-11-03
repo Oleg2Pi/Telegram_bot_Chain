812a94d0a77381007490a59f2d2a26d154f96947
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-11-03 21:40:46 +0300

    Change HibernateUtil: del all; create field sessionFactory create static block, where create config, add objects class, create sessionFactory create methods: getSession and closeSessionFactory

--- src/main/java/by/polikarpov/utils/HibernateUtil.java
+++ src/main/java/by/polikarpov/utils/HibernateUtil.java
@@ -1,15 +1,33 @@
 package by.polikarpov.utils;
 
 import by.polikarpov.entity.Person;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
 
-    private static final Configuration config = new Configuration().configure();
+    private static final SessionFactory sessionFactory;
+
+    static {
+        try {
+            Configuration config = new Configuration().configure();
+            config.addAnnotatedClass(Person.class);
+            sessionFactory = config.buildSessionFactory();
+        } catch (HibernateException e) {
+            System.err.println("Initial SessionFactory creation failed: " + e);
+            throw new RuntimeException(e);
+        }
+    }
 
     public static Session getSession() {
-        config.addAnnotatedClass(Person.class);
-        return config.buildSessionFactory().openSession();
+        return sessionFactory.openSession();
+    }
+
+    public static void closeSessionFactory() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
     }
 }
