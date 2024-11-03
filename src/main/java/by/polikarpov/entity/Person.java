7624ac896b13643cd41b586b1c113663285b35f6
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-11-03 21:29:35 +0300

    Create object data for Person

---
+++ src/main/java/by/polikarpov/entity/Person.java
@@ -0,0 +1,27 @@
+package by.polikarpov.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+    private String usernameTG;
+    private String phone;
+}
