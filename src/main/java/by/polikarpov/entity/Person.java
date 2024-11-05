22800f7478719d8473d2bb7726821900069f700f
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-11-05 10:16:08 +0300

    Clear in Person: del serializable

--- src/main/java/by/polikarpov/entity/Person.java
+++ src/main/java/by/polikarpov/entity/Person.java
@@ -6,15 +6,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
-
 @Entity
 @Table(name = "person")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Person implements Serializable {
+public class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
