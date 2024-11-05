c07f7af90ac900a16c13b0d64bc5409ae86de71d
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-11-05 08:24:50 +0300

    Add image in PersonDao

--- src/main/java/by/polikarpov/repository/PersonDao.java
+++ src/main/java/by/polikarpov/repository/PersonDao.java
@@ -1,17 +1,19 @@
 package by.polikarpov.repository;
 
+import by.polikarpov.entity.ImagePerson;
 import by.polikarpov.entity.Person;
 import by.polikarpov.utils.HibernateUtil;
 import org.hibernate.Session;
 
 public class PersonDao {
 
-    public void save(Person person) {
+    public void save(Person person, ImagePerson image) {
         Session session = HibernateUtil.getSession();
         try {
             session.beginTransaction();
 
             session.save(person);
+            session.save(image);
 
             session.getTransaction().commit();
         } catch (Exception e) {
